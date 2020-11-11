947e91120f2bc86c1e29d5cb38d4df166e7d4bb4
Dirk Eddelbuettel <edd@debian.org>
2020-11-11 16:04:54 -0600

    add protect/unprotect example and discussion

--- inst/snippets/protectExamples.cpp
+++ inst/snippets/protectExamples.cpp
@@ -29,3 +29,95 @@ SEXP vectorExample() {
 /*** R
 vectorExample()
 */
+
+
+// We use RcppSpdlog to get some insight into the protection under two scenarios
+// [[Rcpp::depends(RcppSpdlog)]]
+
+// define a new 'logging Protect', for simplicity _outside_ the R namespace
+
+#include <RcppSpdlog>
+
+class Protect {
+public:
+    Protect(SEXP obj): sx_(obj) { 					// construct protected SEXP
+        spdlog::info("entered ctor");
+        if (obj != R_NilValue)
+            PROTECT(obj);
+    }
+    ~Protect() {            						// destruct protected SEXP
+        spdlog::info("entered dtor");
+        if (sx_ != R_NilValue)
+            UNPROTECT(1);
+    }
+
+    operator SEXP() const { 						// default access via SEXP operator
+        spdlog::info("entered SEXP()");
+        return sx_;
+    }
+
+    // omitting four delete line here for compactness
+
+    private:
+        SEXP sx_;
+};
+
+
+// We redo the convolution example
+
+// [[Rcpp::export]]
+SEXP convolveIncorrect(SEXP a, SEXP b)
+{
+    auto sp = spdlog::get("R");         					// retrieve existing one
+    if (sp == nullptr) sp = spdlog::r_sink_mt("R");     	// or create new one if needed
+    spdlog::set_default_logger(sp);                         // and set as default
+    spdlog::set_pattern("[%H:%M:%S.%f] %v");
+    spdlog::info("starting convolveIncorrect");
+
+    int na, nb, nab;
+    double *xa, *xb, *xab;
+    SEXP ab;
+
+    a = Protect(R::coerceVectorNumeric(a));
+    b = Protect(R::coerceVectorNumeric(b));
+    na = R::length(a); nb = R::length(b); nab = na + nb - 1;
+    ab = Protect(R::allocVectorNumeric(nab));
+    xa = R::numericPointer(a); xb = R::numericPointer(b); xab = R::numericPointer(ab);
+    for(int i = 0; i < nab; i++) xab[i] = 0.0;
+    for(int i = 0; i < na; i++)
+        for(int j = 0; j < nb; j++) xab[i + j] += xa[i] * xb[j];
+    spdlog::info("ending convolveIncorrect");
+    return ab;
+}
+
+/*** R
+convolveIncorrect(c(1,2,3), c(4,5,6))
+*/
+
+// [[Rcpp::export]]
+SEXP convolveCorrect(SEXP a, SEXP b)
+{
+    auto sp = spdlog::get("R");         					// retrieve existing one
+    if (sp == nullptr) sp = spdlog::r_sink_mt("R");     	// or create new one if needed
+    spdlog::set_default_logger(sp);                         // and set as default
+    spdlog::set_pattern("[%H:%M:%S.%f] %v");
+    spdlog::info("starting convolveCorrect");
+
+    int na, nb, nab;
+    double *xa, *xb, *xab;
+
+    Protect pa(R::coerceVectorNumeric(a));
+    Protect pb(R::coerceVectorNumeric(b));
+    na = R::length(pa); nb = R::length(pb); nab = na + nb - 1;
+    Protect ab(R::allocVectorNumeric(nab));
+    xa = R::numericPointer(pa); xb = R::numericPointer(pb); xab = R::numericPointer(ab);
+    for(int i = 0; i < nab; i++) xab[i] = 0.0;
+    for(int i = 0; i < na; i++)
+        for(int j = 0; j < nb; j++) xab[i + j] += xa[i] * xb[j];
+    spdlog::info("ending convolveCorrect");
+    return ab;
+}
+
+/*** R
+convolveCorrect(c(1,2,3), c(4,5,6))
+*/
