fa259377b9119ef04d2d10f405711f9333984cc6
Dirk Eddelbuettel <edd@debian.org>
2021-08-29 11:26:47 -0500

    remove numvec from example snippet

--- inst/snippets/rollminmaxClass.cpp
+++ inst/snippets/rollminmaxClass.cpp
@@ -18,32 +18,6 @@ struct Args {
     CalcType ctype;
 };
 
-class NumVec {
-public:
-    NumVec(SEXP s_): s(s_) {};
-    NumVec(const int n): s(R::allocVectorNumeric(n)) {};
-    NumVec(const NumVec &) = default;						// copy ctor
-    NumVec(NumVec &&) noexcept = default;					// move ctor
-    NumVec& operator=(const NumVec &) = default;            // copy assignment
-    NumVec& operator=(NumVec &&) noexcept = default;        // move assignment
-    ~NumVec() noexcept = default;                           // dtor
-
-    operator SEXP() const { return s; };
-
-    class Proxy {
-    public:
-        Proxy(SEXP s_, int index_) : s(s_), index(index_) {}
-        Proxy& operator=(double x) { R::numericPointer(s)[index] = x; return *this; }
-        inline operator double() { return R::numericPointer(s)[index]; }
-        SEXP s;
-        int index;
-    };
-    inline Proxy operator[](int i) { return Proxy(s, i); }
-
-private:
-    SEXP s;
-};
-
 // calculates rolling window for {minimum, maximum}
 NumVec roll_minmax(NumVec x, Args a) {
     int n  = R::length(x);
